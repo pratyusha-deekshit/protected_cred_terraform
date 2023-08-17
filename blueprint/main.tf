636b71f1ff1708e4402b854433b63ca12f2ca237
Deekshit <pratyusha.deekshit@accenture.com>
2023-08-17 16:58:00 +0530

    first commit

---
+++ blueprint/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    genesyscloud = {
+      source  = "mypurecloud/genesyscloud",
+      version = ">= 1.6.0"
+    }
+  }
+}
+
+provider "genesyscloud" {
+  features {}
+}
+
+
+resource "genesyscloud_tf_export" "export" {
+  directory          = "./genesyscloud"
+  export_as_hcl      = true
+  resource_types     = ["genesyscloud_user"]
+  include_state_file = true
+}
