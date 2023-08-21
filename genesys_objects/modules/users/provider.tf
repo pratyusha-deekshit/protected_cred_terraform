0dc0cc2ca1475ff761ffe4786343593b6df76ef2
pratyusha-deekshit <126081699+pratyusha-deekshit@users.noreply.github.com>
2023-08-21 16:30:31 +0530

    Create provider.tf

---
+++ genesys_objects/modules/users/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    genesyscloud = {
+      source  = "mypurecloud/genesyscloud",
+      version = ">= 1.6.0"
+    }
+  }
+}
+
+provider "genesyscloud" {
+}
