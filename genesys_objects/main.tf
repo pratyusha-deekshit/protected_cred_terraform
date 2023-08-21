e157538b26b25136bada6de0f11df4638a49bb51
Deekshit <pratyusha.deekshit@accenture.com>
2023-08-21 16:20:29 +0530

    new commit

--- genesys_objects/main.tf
+++ genesys_objects/main.tf
@@ -1,16 +1,6 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    genesyscloud = {
-      source  = "mypurecloud/genesyscloud",
-      version = ">= 1.6.0"
-    }
-  }
-}
-
-provider "genesyscloud" {
-}
-
-module "classifier_users" {
-  source = "./modules/users"
+resource "genesyscloud_tf_export" "export" {
+  directory          = "./genesyscloud"
+  export_as_hcl      = true
+  resource_types     = ["genesyscloud_user"]
+  include_state_file = true
 }
